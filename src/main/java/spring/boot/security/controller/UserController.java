21f347a71fc7b001c4b1c8163162ebb7d09aeb78
Teka-Dev <teka.proger@gmail.com>
2022-07-28 22:52:25 +0500

    add ResponseEntity, eraseCredentials()

--- src/main/java/spring/boot/security/controller/UserController.java
+++ src/main/java/spring/boot/security/controller/UserController.java
@@ -1,24 +1,12 @@
 package spring.boot.security.controller;
 
+import org.springframework.security.core.Authentication;
 import spring.boot.security.entity.User;
-import spring.boot.security.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import java.security.Principal;
-import java.util.List;
 
 @Controller
 public class UserController {
-
-    private final UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
 
     @GetMapping("/user")
     public String getUserInformationPage() {
@@ -27,9 +15,9 @@ public class UserController {
 
     @ResponseBody
     @GetMapping("/current-user")
-    public User getCurrentUser(Principal principal) {
-        User currentUser = userService.findByUsername(principal.getName());
-        currentUser.setPassword(null);
+    public User getCurrentUser(Authentication authentication) {
+        User currentUser = (User) authentication.getPrincipal();
+        currentUser.eraseCredentials();
         return currentUser;
     }
 }
